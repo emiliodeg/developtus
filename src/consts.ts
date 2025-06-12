da34ea51472e7edd63782e44a4e9608a4540d4d8
Emilio <1172386+emiliodeg@users.noreply.github.com>
2025-06-12 18:01:46 -0300

    feat: enhance social links in Aside component with icons and styles

--- src/consts.ts
+++ src/consts.ts
@@ -5,20 +5,27 @@ export const SITE_TITLE = "Emilio Degiovanni";
 export const SITE_DESCRIPTION = "Welcome to my website!";
 export const SOCIAL_LINKS = [
   {
-    label: "GitHub",
-    href: "https://github.com/emiliodeg",
-  },
-  {
     label: "LinkedIn",
     href: "https://www.linkedin.com/in/emiliodeg/",
+    icon: 'linkedin',
+    color: '#0a66c2'
   },
   {
     label: "Mail me",
     href: "mailto:emiliodeg@gmail.com",
+    icon: 'email',
+    color: '#dd4b39'
   },
   {
     label: "My resume",
     href: "/cv-english.pdf",
     download: "Emilio Degiovanni Resume.pdf",
+    icon: 'download'
+  },
+  {
+    label: "GitHub",
+    href: "https://github.com/emiliodeg",
+    icon: 'github',
+    color: '#4078c0'
   },
 ];
