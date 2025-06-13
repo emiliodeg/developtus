32c7752db1229aff19196ba8a7fa5d7967212670
Emilio <1172386+emiliodeg@users.noreply.github.com>
2025-06-13 11:38:59 -0300

    feat: update robots.txt with correct sitemap URL; enhance Aside component styles and social links; refine global styles for improved theming

--- src/consts.ts
+++ src/consts.ts
@@ -3,30 +3,32 @@
 
 export const SITE_TITLE = "Emilio Degiovanni";
 export const SITE_TITLE_HERO = "Emilio Degiovanni - Senior Frontend Engineer";
-export const SITE_DESCRIPTION = "Angular, Material & TypeScript specialist, and Apps that performs and scales. I build fast, scalable, and modern web apps using Angular.";
+export const SITE_DESCRIPTION =
+  "Angular, Material & TypeScript specialist, and Apps that performs and scales. I build fast, scalable, and modern web apps using Angular.";
 export const SOCIAL_LINKS = [
   {
     label: "LinkedIn",
     href: "https://www.linkedin.com/in/emiliodeg/",
-    icon: 'linkedin',
-    color: '#0a66c2'
+    icon: "linkedin",
+    color: "10, 102, 194", // #0a66c2
   },
   {
     label: "Mail me",
     href: "mailto:emiliodeg@gmail.com",
-    icon: 'email',
-    color: '#dd4b39'
+    icon: "email",
+    color: "221, 75, 57", // #dd4b39
   },
   {
     label: "My resume",
     href: "/cv-english.pdf",
     download: "Emilio Degiovanni Resume.pdf",
-    icon: 'download'
+    icon: "download",
+    color: "182, 186, 190", // #4078c0
   },
   {
     label: "GitHub",
     href: "https://github.com/emiliodeg",
-    icon: 'github',
-    color: '#4078c0'
+    icon: "github",
+    color: "64, 120, 192", // #4078c0
   },
 ];
